8075e0c2f4c9e13bace81fb1dfe012d4186c3591
유아영 <ahyoung.db@gmail.com>
2022-08-09 22:50:54 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chatting/precache-manifest.176c56c5c0133ff0f6fbf5119427fe53.js"
+  "/chatting/precache-manifest.54afa8330b90821709fba141696098c6.js"
 );
 
 self.addEventListener('message', (event) => {
